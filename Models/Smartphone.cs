0209f177b4366ec937a7b3c1055b3f81f2f66eca
Fabio Senda <fabiohidekisenda@gmail.com>
2022-10-10 18:14:01 -0300

    Desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,19 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string Imei;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        protected List<string> Aplicativos;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            Aplicativos = new List<string>();
         }
 
         public void Ligar()
@@ -22,5 +30,18 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void ListarAplicativosInstalados()
+        {
+            if(Aplicativos.Count == 0){
+                Console.WriteLine("Não há aplicativos instalados.");
+            }
+            else{
+                Console.WriteLine("Aplicativos instalados: ");
+                foreach (string app in Aplicativos){
+                    Console.WriteLine("- " + app);
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia(numero: "12345-6789", modelo: "5.4", imei: "111222333", memoria: 64);
+nokia.Ligar();
+nokia.ListarAplicativosInstalados();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nIphone: ");
+Smartphone iphone = new Iphone(numero: "12345-6789", modelo: "X", imei: "111222333", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.InstalarAplicativo("Facebook");
+iphone.ListarAplicativosInstalados();
